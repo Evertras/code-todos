996eb451f765a9f0ad766f3415442e112aa62eb9
Brandon Fulljames <bfullj@gmail.com>
2023-09-23 14:39:50 +0900

    Filter on actual TODOs

---
+++ internal/todos/file.go
@@ -0,0 +1,36 @@
+package todos
+
+import (
+	"go/parser"
+	"go/token"
+	"strings"
+)
+
+func findTodosInFile(filename string) ([]Todo, error) {
+	todos := make([]Todo, 0)
+
+	fileset := token.NewFileSet()
+
+	parsed, err := parser.ParseFile(fileset, filename, nil, parser.ParseComments)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, comment := range parsed.Comments {
+		text := comment.Text()
+
+		if !strings.Contains(text, "TODO") {
+			continue
+		}
+
+		todos = append(todos, Todo{
+			Filename:    filename,
+			PackageName: parsed.Name.Name,
+			Line:        fileset.Position(comment.Pos()).Line,
+			Text:        text,
+		})
+	}
+
+	return todos, nil
+}
--- internal/todos/todos.go
+++ internal/todos/todos.go
@@ -2,8 +2,6 @@ package todos
 
 import (
 	"fmt"
-	"go/parser"
-	"go/token"
 	"os"
 	"path/filepath"
 )
@@ -73,29 +71,6 @@ func findTodosInPath(path string) ([]Todo, error) {
 
 	if err != nil {
 		return nil, err
-	}
-
-	return todos, nil
-}
-
-func findTodosInFile(filename string) ([]Todo, error) {
-	todos := make([]Todo, 0)
-
-	fileset := token.NewFileSet()
-
-	parsed, err := parser.ParseFile(fileset, filename, nil, parser.ParseComments)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for _, comment := range parsed.Comments {
-		todos = append(todos, Todo{
-			Filename:    filename,
-			PackageName: parsed.Name.Name,
-			Line:        fileset.Position(comment.Pos()).Line,
-			Text:        comment.Text(),
-		})
 	}
 
 	return todos, nil
